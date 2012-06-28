137a05637cd067f3a4b209729419597362e0e2d8
Remco van 't Veer <remco@remworks.net>
2012-06-28 21:12:55 +0200

    handle absence of data more gracefully

--- public/app.js
+++ public/app.js
@@ -5,12 +5,14 @@ var updater = function() {
   setTimeout(updater, 15000)
   $.getJSON("/feed",
             function(data) {
-              $("#date").html(data.report[0].published_at.substr(0, 11))
               var html = ''
-              $.each(data.report, function() {
-                html += '<li><strong>' + this.published_at.substr(11,5) + ' ' + this.title + '</strong><p>' + this.comment + '</p></li>'
-              })
-              $("#comments").html(html)
+              if (data.report.length > 0) {
+                $("#date").html(data.report[0].published_at.substr(0, 11))
+                $.each(data.report, function() {
+                  html += '<li><strong>' + this.published_at.substr(11,5) + ' ' + this.title + '</strong><p>' + this.comment + '</p></li>'
+                })
+                $("#comments").html(html)
+              }
             })
 }
 
