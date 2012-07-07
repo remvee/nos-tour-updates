dd1d7ce7b0463f9a5de4be38de4f984d90601b86
Remco van 't Veer <remco@remworks.net>
2012-07-07 14:52:38 +0200

    yay!  it's y-combinator day!  ;)

--- public/app.js
+++ public/app.js
@@ -1,19 +1,21 @@
-$(document).ajaxStart(function() { $("body").addClass("busy") })
-$(document).ajaxComplete(function() { $("body").removeClass("busy") })
+$(function() {
+  $(document).ajaxStart(function(){$("body").addClass("busy")});
+  $(document).ajaxComplete(function(){$("body").removeClass("busy")});
 
-var updater = function() {
-  setTimeout(updater, 15000)
-  $.getJSON("/feed?" + new Date().getTime(),
-            function(data) {
-              var html = ''
-              if (data.report.length > 0) {
-                $("#date").html(data.report[0].published_at.substr(0, 11))
-                $.each(data.report, function() {
-                  html += '<li><strong>' + this.published_at.substr(11,5) + ' ' + this.title + '</strong><p>' + this.comment + '</p></li>'
-                })
-                $("#comments").html(html)
-              }
-            })
-}
-
-$(updater)
+  (function(x) {
+    return (function(y){return y(y)})(function(y){return x(function(){return y(y)()})});
+  })(function(f) {
+    return function() {
+      setTimeout(f, 15000);
+      $.getJSON("/feed?" + new Date().getTime(),
+                function(data) {
+                  if (data.report.length > 0) {
+                    $("#date").html(data.report[0].published_at.substr(0, 11));
+                    $("#comments").html($.map(data.report, function(v) {
+                      return "<li><strong>" + v.published_at.substr(11,5) + " " + v.title + "</strong><p>" + v.comment + "</p></li>";
+                    }).join(""));
+                  }
+                });
+    }
+  })();
+});
